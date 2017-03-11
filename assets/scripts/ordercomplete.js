02a434221b7cf0c755e39b4aeb1cd46144d10404
akgoode <akgoode@gmail.com>
2017-03-11 15:06:12 -0500

    This successfully resets the orderComplete id.
    Solved by turning it into an object that has a setter and a getter
    as properties.  This allows the value in ID to persist between files.
    
    Now after a successful checkout a new order begins.  The user can
    still patch their order as before until they successfully checkout.
    
    Tri programmed with Lauren and Mario

--- assets/scripts/ordercomplete.js
+++ assets/scripts/ordercomplete.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = '';
+const orderStatus = {
+  id: '',
+  setId: function(num) {
+    this.id = num;
+  },
+  getId: function() {
+    return this.id;
+  }
+};
+
+module.exports = orderStatus;
