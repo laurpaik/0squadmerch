3cfd49b58eedb3a7ff77522000f209ff8468b7ce
akgoode <akgoode@gmail.com>
2017-03-14 12:08:12 -0400

    add user token to stripe api call

--- assets/scripts/stripe/api.js
+++ assets/scripts/stripe/api.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const config = require('../config');
+const store = require('../store');
 
 const createCharge = function (data) {
   return $.ajax({
     url: config.apiOrigin + '/charges',
     method: 'POST',
+    headers: {
+      Authorization: `Token token=${store.user.token}`,
+    },
     data,
   });
 };
